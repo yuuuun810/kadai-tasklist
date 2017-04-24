938fb048ac8ad8db7d13baea270e83a89cae4c1a
yurihayashi <yuuuun810@yahoo.co.jp>
2017-04-24 05:58:18 +0000

    add column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -56,6 +56,6 @@ class MessagesController < ApplicationController
   end
   
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :status)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
   validates :status, presence: true, length: { maximum: 10 }
+ 
 end
